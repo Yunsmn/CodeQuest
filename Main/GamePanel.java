424806260ad02b1a2c4859cb2e0b3667681f0590
Yunsmn <yunsmn@example.com>
2025-11-23 11:24:40 +0100

    sea like background

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -43,7 +43,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.black);
+        this.setBackground(new Color(0x156c99));
         this.setLayout(null);
         this.setFocusable(false);
         this.setVisible(true);
